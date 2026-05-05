53cbeda54ac0537e8f27e83b1fa3937c79a6e7c4
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-05 19:41:38 +0700

    menambahkan consructor dipanggil

--- dectructor.cpp
+++ dectructor.cpp
@@ -51,3 +51,11 @@ void angka::isiData()
     }
     cout << endl;
 }
+int main()
+{
+    angka belajarcpp(3);
+    angka "ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp";
+
+    return 0;
+}
