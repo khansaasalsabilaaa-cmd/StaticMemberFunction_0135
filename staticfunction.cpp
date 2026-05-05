a00ee77da360d23da381f50283232be92b3ebc07
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-05 18:39:05 +0700

    menambahkan pembuatan static function

---
+++ staticfunction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa{
+
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim){
+        nim = pnim;
+    };
+
+    static int getNim(){
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama){
+        setID();
+    };
+};
+
